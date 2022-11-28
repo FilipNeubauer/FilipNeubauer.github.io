651a93b7ac8059f2646cd5640f2267ba0e79b798
FilipNeubauer <filipneub@seznam.cz>
2022-11-28 17:09:36 +0100

    basic light box, one font

--- index.js
+++ index.js
@@ -26,6 +26,29 @@ function mediaQuery(x) {
   }
 
 
+
+function lightBox() {
+    const images = $(".photos");
+    // console.log(images.length);
+    for (let i = 0; i<images.length; i++) {
+        images[i].onclick = () => {
+            $(".light-box .image-box img").attr("src", images[i].src);
+            $(".back").css("visibility", "visible");
+        }
+    }
+}
+
+function closeL() {
+    $(".back").css("visibility", "hidden");
+    $(".light-box .image-box img").attr("src", "");
+
+}
+
+
+lightBox();
+$(".back").bind("click", closeL);
+
+
 var x = window.matchMedia("(min-width: 768px)");
 mediaQuery(x);
 x.addListener(mediaQuery);
